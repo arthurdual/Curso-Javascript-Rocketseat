0658fe4b7231cd44a25d9d0a354533b98cd5160b
unknown <arthurdual2014@hotmail.com>
2019-09-11 20:20:43 -0300

    salvando arquivos do curso javascript rocketseat

---
+++ Módulo 3 App de Todos/Aula 17 Renderizando todos/todos.js
@@ -0,0 +1,23 @@
+//alert('ok');
+var listElement  = document.querySelector('#app ul');
+var inputElment  = document.querySelector('#app input');
+var buttonElment = document.querySelector('#app button');
+
+var todos = [
+    'Fazer tarefas 01',
+    'Estuadr JS',
+    'Participar da comunidade JS'
+];
+
+function renderTodos(){
+    for(todo of todos){
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+
+        todoElement.appendChild(todoText);
+        listElement.appendChild(todoElement);
+
+    }
+}
+
+renderTodos();
---
+++ Módulo 4 JS assíncrono/Aula 22 Promises/main.js
@@ -0,0 +1,25 @@
+var minhaPrimise = function(){
+    return new Promise(function(resolve,reject){
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://apii.github.com/users/#');
+        xhr.send(null);
+
+        xhr.onreadystatechange = function(){
+        if(xhr.readyState === 4){
+            if(xhr.status === 200){
+                resolve(JSON.parse(xhr.responseText));
+            }else{
+                reject('ERROR NA REQUISIÇÃO');
+            }
+        }
+    }   
+    });
+}
+
+minhaPrimise()
+    .then(function(response){
+        console.log(response);
+    })
+    .catch(function(error){
+        console.warn(error)
+    });
